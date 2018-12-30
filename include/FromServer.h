5b9cb8ae2b34d8942cc1833684d01a96d59cd0e3
maorrocky <maorrocky@gmail.com>
2018-12-30 23:33:16 +0200

    first commit

---
+++ include/FromServer.h
@@ -0,0 +1,10 @@
+#include "connectionHandler.h"
+
+#ifndef BOOST_ECHO_CLIENT_FROMSERVER_H
+#define BOOST_ECHO_CLIENT_FROMSERVER_H
+
+class FromServer {
+    
+};
+
+#endif //BOOST_ECHO_CLIENT_FROMSERVER_H
